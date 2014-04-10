ea60f31699a52e7bb3ef9ac68ed0844c1689f26e
Jamie Oaks <joaks1@gmail.com>
2014-04-09 18:20:14 -0700

    Updating `pull-images.sh` script.

--- images/pull-images.sh
+++ images/pull-images.sh
@@ -11,4 +11,6 @@ else
 fi
 
 curl -o darwin-tol-copyright-boris-kulikov-2007.jpg http://www.boriskulikov.com/images/DarwinTreeOfLife-%20L.jpg
+curl -o correlation.mov http://phylo.bio.ku.edu/BIOL428/correl-anim2.mov
+curl -o no-correlation.mov http://phylo.bio.ku.edu/BIOL428/no-correl-anim.mov
 
